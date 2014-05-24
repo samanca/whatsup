01dd201632706fb7ebf4ddb939b3657675a7bebc
Amirsaman Memaripour <amemarip@eng.ucsd.edu>
2014-05-24 16:16:05 -0700

    updated scripts

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ COUNTER=0
 while [ $COUNTER -lt $MAX_CLIENTS ]; do
     let COUNTER=COUNTER+1
     USER=$COUNTER
-    PASSWORD="root"
+    PASSWORD="jack"
+    #PASSWORD="root"
     ./client_bot.sh "$USER" "$PASSWORD" "$MAX_CLIENTS" | java -cp client/dist/lib/smack-3.4.1-0cec571.jar:client/dist/lib/client.jar:client/dist/lib/smackx-3.4.1-0cec571.jar com.ucsd.whatsup.Main 1>"stdout/${COUNTER}.txt" 2>"stderr/${COUNTER}.txt" &
 done

8d3064eeb69aaf19be8c43d209c5e801b943d90b
Amirsaman Memaripour <amemarip@eng.ucsd.edu>
2014-05-24 18:38:40 -0700

    updated run script

--- run.sh
+++ run.sh
@@ -18,6 +18,6 @@ while [ $COUNTER -lt $MAX_CLIENTS ]; do
     PASSWORD="root"
     #./client_bot.sh "$USER" "$PASSWORD" "$MAX_CLIENTS" | java -cp client/dist/lib/smack-3.4.1-0cec571.jar:client/dist/lib/client.jar:client/dist/lib/smackx-3.4.1-0cec571.jar com.ucsd.whatsup.Main 1>"stdout/${COUNTER}.txt" 2>"stderr/${COUNTER}.txt" &
     #./client_bot.sh "$USER" "$PASSWORD" "$MAX_CLIENTS" | java -cp client/dist/lib/smack-3.4.1-0cec571.jar:client/dist/lib/client.jar:client/dist/lib/smackx-3.4.1-0cec571.jar com.ucsd.whatsup.Main &
-    java -cp client/dist/lib/smack-3.4.1-0cec571.jar:client/dist/lib/client.jar:client/dist/lib/smackx-3.4.1-0cec571.jar com.ucsd.whatsup.Main < "stdin/${COUNTER}.txt" &
-    #./client_bot_2.sh "$USER" "$PASSWORD" "$MAX_CLIENTS" > "stdin/${COUNTER}.txt" 
+    #java -cp client/dist/lib/smack-3.4.1-0cec571.jar:client/dist/lib/client.jar:client/dist/lib/smackx-3.4.1-0cec571.jar com.ucsd.whatsup.Main < "stdin/${COUNTER}.txt" &
+    ./client_bot_2.sh "$USER" "$PASSWORD" "$MAX_CLIENTS" > "stdin/${COUNTER}.txt" 
 done

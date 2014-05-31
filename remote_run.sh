95336a0e3a577be5e3c66605567f6ee6ed53d5ab
Amirsaman Memaripour <amemarip@eng.ucsd.edu>
2014-05-30 20:11:56 -0700

    bug fix

--- remote_run.sh
+++ remote_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for server in "$@"
 do
-    ssh "$server" "nohup ./whatsup/simulator/run.sh ~/whatsup/simulator/${server}.input > /dev/null &"
+    ssh "$server" "nohup ./whatsup/simulator/run.sh ~/whatsup/simulator/${server}.input > output.log &"
 done

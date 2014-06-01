dca433eeeea7465cdf42d36558c12f23c0c971be
Amirsaman Memaripour <amemarip@eng.ucsd.edu>
2014-05-31 18:32:09 -0700

    updated test scripts

--- remote_run.sh
+++ remote_run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# git pull
 for server in "$@"
 do
-    ssh "$server" "nohup ./whatsup/simulator/run.sh /home/ubuntu/whatsup/simulator/${server}.input 1>output.log 2>error.log &"
+    ssh "$server" "cd /home/ubuntu/whatsup && git pull"
 done
+
+# run clients
+#for server in "$@"
+#do
+#    ssh "$server" "nohup ./whatsup/simulator/run.sh /home/ubuntu/whatsup/simulator/${server}.input 1>output.log 2>error.log &"
+#done

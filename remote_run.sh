7395acabe3d3a04b87a04c736836335f689b0af9
Amirsaman Memaripour <amemarip@eng.ucsd.edu>
2014-05-31 18:24:18 -0700

    updated test scripts

--- remote_run.sh
+++ remote_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for server in "$@"
 do
-    ssh "$server" "nohup ./home/ubuntu/simulator/run.sh /home/ubuntu/simulator/${server}.input 1>output.log 2>error.log &"
+    ssh "$server" "nohup ./whatsup/simulator/run.sh /home/ubuntu/whatsup/simulator/${server}.input 1>output.log 2>error.log &"
 done

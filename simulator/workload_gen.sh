96824d741a6873ce8e232bfa306ea4c48d0d30df
Amirsaman Memaripour <amemarip@eng.ucsd.edu>
2014-06-01 22:39:20 -0700

    1600 users

--- simulator/workload_gen.sh
+++ simulator/workload_gen.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-START=$1
-END=$2
+USERS_COUNT=$1
+AMAZON_INSTANCES=$2
 
-COUNTER=$START
-while [ $COUNTER -le $END ]; do
-    echo "${COUNTER}|root"
+let LOAD_PER_INSTANCE=USERS_COUNT/AMAZON_INSTANCES
+COUNTER=1
+
+rm ec2-*
+
+while [ $COUNTER -le $AMAZON_INSTANCES ]; do
+    INSTANCE="ec2-$COUNTER"
+    let START=(COUNTER-1)*LOAD_PER_INSTANCE+1
+    let END=(COUNTER)*LOAD_PER_INSTANCE
+    C2=$START
+    while [ $C2 -le $END ]; do
+        echo "${C2}|root" >> "${INSTANCE}.input"
+        let C2=C2+1
+    done
     let COUNTER=COUNTER+1
 done
